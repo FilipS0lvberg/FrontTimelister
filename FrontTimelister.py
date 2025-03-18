29c3f6e667901b56648adb3a9e1bd820cb125058
FilipS0lvberg <fs@frontinnovation.no>
2025-03-18 12:18:42 +0100

    pushing change to the year

--- FrontTimelister.py
+++ FrontTimelister.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from supporting_functions import *
-from create_workbook import create_workbook1
+from create_timelister import create_timelister1
 
 
 st.title("Front Generation - Timelister :memo:")
@@ -10,7 +10,7 @@ if "flag" not in st.session_state:
     st.session_state.flag = False
 
 
-year = st.number_input("Årstall", min_value=2025, max_value=2035, value=2025)
+year = st.number_input("Årstall", min_value=2020, max_value=2050, value=2025)
 selskapsnavn = st.text_input("Selskapsnavn")
 prosjekttittel = st.text_input("Prosjekttittel")
 prosjektleder = st.text_input("Prosjektleder")
@@ -31,7 +31,7 @@ if st.session_state.flag:
         type="primary",
         label="Last ned Q1 - Timelister",
         # data=write_docx(st.session_state.norsk_proj_title, st.session_state.main_checkboxes ,st.session_state.main_box_contents, st.session_state.workpackages),
-        data=create_workbook1(year=year, quarter=1, selskap=selskapsnavn, prosjekttittel=prosjekttittel, prosjektnummer=prosjektnummer, prosjektleder=prosjektleder, prosject_description=prosjektbeskrivelse, workers=get_workers(ant_ansatte), num_workpackages=antall_arbeidspakker),
+        data=create_timelister1(year=year, quarter=1, selskap=selskapsnavn, prosjekttittel=prosjekttittel, prosjektnummer=prosjektnummer, prosjektleder=prosjektleder, prosject_description=prosjektbeskrivelse, workers=get_workers(ant_ansatte), num_workpackages=antall_arbeidspakker),
         file_name=f"SF{year} - Timelister Q1 - {selskapsnavn}",
         mime="xlsx"
     )
@@ -40,7 +40,7 @@ if st.session_state.flag:
         type="primary",
         label="Last ned Q2 - Timelister",
         # data=write_docx(st.session_state.norsk_proj_title, st.session_state.main_checkboxes ,st.session_state.main_box_contents, st.session_state.workpackages),
-        data=create_workbook1(year=year, quarter=2, selskap=selskapsnavn, prosjekttittel=prosjekttittel, prosjektnummer=prosjektnummer, prosjektleder=prosjektleder, prosject_description=prosjektbeskrivelse, workers=get_workers(ant_ansatte), num_workpackages=antall_arbeidspakker),
+        data=create_timelister1(year=year, quarter=2, selskap=selskapsnavn, prosjekttittel=prosjekttittel, prosjektnummer=prosjektnummer, prosjektleder=prosjektleder, prosject_description=prosjektbeskrivelse, workers=get_workers(ant_ansatte), num_workpackages=antall_arbeidspakker),
         file_name=f"SF{year} - Timelister Q2 - {selskapsnavn}",
         mime="xlsx"
     )
@@ -49,7 +49,7 @@ if st.session_state.flag:
         type="primary",
         label="Last ned Q3 - Timelister",
         # data=write_docx(st.session_state.norsk_proj_title, st.session_state.main_checkboxes ,st.session_state.main_box_contents, st.session_state.workpackages),
-        data=create_workbook1(year=year, quarter=3, selskap=selskapsnavn, prosjekttittel=prosjekttittel, prosjektnummer=prosjektnummer, prosjektleder=prosjektleder, prosject_description=prosjektbeskrivelse, workers=get_workers(ant_ansatte), num_workpackages=antall_arbeidspakker),
+        data=create_timelister1(year=year, quarter=3, selskap=selskapsnavn, prosjekttittel=prosjekttittel, prosjektnummer=prosjektnummer, prosjektleder=prosjektleder, prosject_description=prosjektbeskrivelse, workers=get_workers(ant_ansatte), num_workpackages=antall_arbeidspakker),
         file_name=f"SF{year} - Timelister Q3 - {selskapsnavn}",
         mime="xlsx"
     )
@@ -58,7 +58,7 @@ if st.session_state.flag:
         type="primary",
         label="Last ned Q4 - Timelister",
         # data=write_docx(st.session_state.norsk_proj_title, st.session_state.main_checkboxes ,st.session_state.main_box_contents, st.session_state.workpackages),
-        data=create_workbook1(year=year, quarter=4, selskap=selskapsnavn, prosjekttittel=prosjekttittel, prosjektnummer=prosjektnummer, prosjektleder=prosjektleder, prosject_description=prosjektbeskrivelse, workers=get_workers(ant_ansatte), num_workpackages=antall_arbeidspakker),
+        data=create_timelister1(year=year, quarter=4, selskap=selskapsnavn, prosjekttittel=prosjekttittel, prosjektnummer=prosjektnummer, prosjektleder=prosjektleder, prosject_description=prosjektbeskrivelse, workers=get_workers(ant_ansatte), num_workpackages=antall_arbeidspakker),
         file_name=f"SF{year} - Timelister Q4 - {selskapsnavn}",
         mime="xlsx"
     )
